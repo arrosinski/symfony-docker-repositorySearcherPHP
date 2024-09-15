5fc10adc73438501b16887f474369ccbd27f495a
arkadiuszrosinski <ar.rosinski@gmail.com>
2024-09-15 10:40:29 +0200

    main: add doc for api

--- src/Controller/Api/RestApiController.php
+++ src/Controller/Api/RestApiController.php
@@ -4,12 +4,57 @@ namespace App\Controller\Api;
 
 use App\SearchInRepo\GithubSearchCodeInRepoStrategy;
 use App\Service\Api\SearchCodeInRepoService;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class RestApiController
 {
-
+    /**
+     * @Route("/api/v1/code/search", methods={"GET"})
+     * @OA\Get(
+     *     path="/api/v1/code/search",
+     *     summary="Search code in repository",
+     *     @OA\Parameter(
+     *         name="code",
+     *         in="query",
+     *         description="Code to search for",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Parameter(
+     *         name="page",
+     *         in="query",
+     *         description="Page number",
+     *         required=false,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Parameter(
+     *         name="per_page",
+     *         in="query",
+     *         description="Number of results per page",
+     *         required=false,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="items", type="array", @OA\Items(
+     *                 type="object",
+     *                 @OA\Property(property="repository", type="object", @OA\Property(property="owner", type="object", @OA\Property(property="login", type="string"))),
+     *                 @OA\Property(property="name", type="string"),
+     *                 @OA\Property(property="score", type="number", format="float")
+     *             ))
+     *         )
+     *     )
+     * )
+     * @Security(name="Bearer")
+     */
     public function searchCodeInRepo(Request $request): JsonResponse
     {
         $searchInCodeReposeService = new SearchCodeInRepoService(new GithubSearchCodeInRepoStrategy());
